0a2a6c35fa95d7c644f46a7cdebc21d542e1ea6c
Alexandre Simoes Tavares <13024466+alex-smtv@users.noreply.github.com>
2022-01-06 18:16:13 +0100

    Add access to the root script path in FreePieVars

--- src/helpers/freepie_vars.py
+++ src/helpers/freepie_vars.py
@@ -1,8 +1,11 @@
 # encoding: utf-8
 from src.helpers.freepie_helper import KeyExtended
 
-# Cette classe va servir de pivot pour avoir accès aux variables keyboard etc. sans devoir transférer les instances à chaque fois à toutes les functions!
+# This class will serve as a pivotal point to access useful variables provided within FreePie without the need to jungle instances around.
 class FreePieVars(object):
+    # Last char doesn't contain any / or \
+    root_script_path = None
+
     joysticks   = None
     vjoys       = None
     keyboard    = None
@@ -15,14 +18,24 @@ class FreePieVars(object):
 
     # TODO: None checking with properties and setter
     @staticmethod
-    def feed_with(joysticks, vjoys, keyboard, Key, diagnostics, speech):
-        FreePieVars.joysticks   = joysticks
-        FreePieVars.vjoys       = vjoys
-        FreePieVars.keyboard    = keyboard
-        FreePieVars.Key         = Key
-        FreePieVars.KeyE        = KeyExtended
-        FreePieVars.diagnostics = diagnostics
-        FreePieVars.speech      = speech
+    def feed_with(root_script_path, joysticks, vjoys, keyboard, Key, diagnostics, speech):
+        # TODO: handle case of duplicate separator (not very important, but more clean especially for printing)
+
+        # Remove the last separator if it exists in script path
+        last_char_index = len(root_script_path)-1
+        last_char = root_script_path[last_char_index]
+        
+        if last_char == "\\" or last_char == "/":
+            root_script_path = root_script_path[:last_char_index]
+
+        FreePieVars.root_script_path = root_script_path.replace("/", "\\")
+        FreePieVars.joysticks        = joysticks
+        FreePieVars.vjoys            = vjoys
+        FreePieVars.keyboard         = keyboard
+        FreePieVars.Key              = Key
+        FreePieVars.KeyE             = KeyExtended
+        FreePieVars.diagnostics      = diagnostics
+        FreePieVars.speech           = speech
 
         _is_all_set = True
 
